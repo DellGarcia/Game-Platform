3281ae3a638ac5066daec7a366015db66c22b8e0
Lucas Del Puerto <lucasgarciadragon@hotmail.com>
2020-07-03 12:36:41 -0300

    :fireworks: adding register view

---
+++ Game-Platform/Models/Player.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game_Platform.Models
+{
+    class Player
+    {
+        private string Username { get; set; }
+        private string Email { get; set; }
+        private string Password { get; set; }
+        private int Score { get; set; }
+        private List<Player> Friends { get; }
+
+    }
+}
